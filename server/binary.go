375b2895004683c3247b2185556a775a6e38028e
Aqil F <aqilfaizal2004@gmail.com>
2026-06-03 18:29:58 -0700

    reading by offset

--- server/binary.go
+++ server/binary.go
@@ -49,6 +49,7 @@ func checksum(entry LogEntry) uint32 {
 }
 
 // append [4-byte length][4-byte checksum][payload bytes] to binary file
+// and record the entry's byte offset in logs.idx
 func appendEntry(entry LogEntry) {
 	file, err := os.OpenFile("logs.bin", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -56,10 +57,48 @@ func appendEntry(entry LogEntry) {
 	}
 	defer file.Close()
 
+	// Seek to end to find where this entry will land before writing
+	offset, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	data := serialize(entry)
 	binary.Write(file, binary.LittleEndian, uint32(len(data)))
 	binary.Write(file, binary.LittleEndian, checksum(entry))
 	file.Write(data)
+
+	appendToIndex(offset)
+}
+
+// appendToIndex records a byte offset as the next entry in logs.idx
+func appendToIndex(offset int64) {
+	file, err := os.OpenFile("logs.idx", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	binary.Write(file, binary.LittleEndian, offset)
+}
+
+// readEntryByIndex looks up entry n in logs.idx, then jumps directly to it in logs.bin
+func readEntryByIndex(n int64) (LogEntry, error) {
+	idxFile, err := os.OpenFile("logs.idx", os.O_RDONLY, 0644)
+	if err != nil {
+		return LogEntry{}, fmt.Errorf("index not found: %w", err)
+	}
+	defer idxFile.Close()
+
+	if _, err := idxFile.Seek(n*8, io.SeekStart); err != nil {
+		return LogEntry{}, fmt.Errorf("seek to index %d failed: %w", n, err)
+	}
+
+	var offset int64
+	if err := binary.Read(idxFile, binary.LittleEndian, &offset); err != nil {
+		return LogEntry{}, fmt.Errorf("index entry %d not found: %w", n, err)
+	}
+
+	return readEntry(offset)
 }
 
 
@@ -104,15 +143,16 @@ func readEntry(offset int64) (LogEntry, error) {
 func main() {
 	writeEntry("This is an important log", 2)
 	writeEntry("This is not so important and is more informational", 1)
-	data, err := readEntry(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(data.Message))
-	data, err = readEntry(41)
+
+	data, err := readEntryByIndex(0)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(data.Message))
 
+	data, err = readEntryByIndex(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data.Message))
 }
